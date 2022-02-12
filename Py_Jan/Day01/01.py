e8b1158ff85ae761edc921710f8f4936c5c8ccfc
Mahpyt <99193976+Mahpyt@users.noreply.github.com>
2022-02-12 08:56:56 -0500

    Add files via upload

---
+++ Py_Jan/Day01/01.py
@@ -0,0 +1,21 @@
+# Texual Data-----> Strings
+
+
+message='Hello'
+print('message')
+
+
+
+message='Hello'
+name='john'
+age='26'
+#greeting=message+''+name
+greeting='{} ,{},{}:'.format(message, name,age)
+greeting=f"{message},{name},{age}!"
+
+print(greeting)
+#greeting=message+name
+message="Hello"
+name="Tom"
+greeting=f"{message},{name}!"
+print(greeting)
---
+++ Py_Jan/Day02/02.py
@@ -0,0 +1,22 @@
+#Whole Number----> Integers
+# Deciamals ---> floats
+# +
+# -
+# *
+# /
+# //
+# **
+# %
+
+# num='3.14'
+
+print(3**2)
+# print(type(num))
+num = 1
+# num = num+1
+num+= 1
+print(num)
+print(round(3.75,1))
+num_1 = 2
+num_2 = 2
+print(num_1==num_2)
